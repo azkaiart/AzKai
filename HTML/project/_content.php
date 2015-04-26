1a153ef35f44a5f156f3cad4c20e93033a7af93e
Kai <kai@byroa.de>
2015-04-26 17:44:17 -0400

    Update to API

--- HTML/project/_content.php
+++ HTML/project/_content.php
@@ -1,72 +1,74 @@
-<!-- PAGE HEADER : begin -->
-<div id="page-header">
-    <div class="container">
-        <h1>List On Left</h1>
-        <ul class="breadcrumbs">
-            <li><a href="index.html">Home</a></li>
-            <li><a href="portfolio-list-grid.html">Portfolio</a></li>
-            <li>List On Left</li>
-        </ul>
-    </div>
-</div>
-<!-- PAGE HEADER : end -->
+<script id="portfolio-template" type="text/x-handlebars-template">
 
-<!-- PAGE CONTENT : begin -->
-<div id="page-content">
-
-    <!-- PORTFOLIO DETAIL : begin -->
-    <div class="portfolio-detail">
-
+    <!-- PAGE HEADER : begin -->
+    <div id="page-header">
         <div class="container">
-            <div class="row">
-                <div class="col-md-8">
+            <h1>List On Left</h1>
+            <ul class="breadcrumbs">
+                <li><a href="index.html">Home</a></li>
+                <li><a href="portfolio-list-grid.html">Portfolio</a></li>
+                <li>List On Left</li>
+            </ul>
+        </div>
+    </div>
+    <!-- PAGE HEADER : end -->
 
-                    <!-- PROJECT IMAGES : begin -->
-                    <div class="project-images">
-                        <p><a href="images/project_detail_01_medium.jpg" class="lightbox"><img src="images/project_detail_01_medium.jpg" title="Epic Armory: Lancer" alt=""></a></p>
-                        <p><a href="images/project_detail_02_medium.jpg" class="lightbox"><img src="images/project_detail_02_medium.jpg" title="Epic Armory: N7 Crusader Shotgun" alt=""></a></p>
+    <!-- PAGE CONTENT : begin -->
+    <div id="page-content">
+
+        <!-- PORTFOLIO DETAIL : begin -->
+        <div class="portfolio-detail">
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8">
+
+                        <!-- PROJECT IMAGES : begin -->
+                        <div class="project-images">
+                            <p><img src="images/project_detail_01_medium.jpg" title="Epic Armory: Lancer" alt=""></p>
+                        </div>
+                        <!-- PROJECT IMAGES : end -->
+
                     </div>
-                    <!-- PROJECT IMAGES : end -->
+                    <div class="col-md-4">
 
-                </div>
-                <div class="col-md-4">
+                        <!-- PROJECT INFO : begin -->
+                        <dl class="project-info">
+                            <dt>Author</dt>
+                            <dd><a href="http://justinmezzell.com/">Justin Mezzell</a></dd>
+                            <dt>Source</dt>
+                            <dd><a href="https://dribbble.com/JustinMezzell">dribbble.com</a></dd>
+                        </dl>
+                        <!-- PROJECT INFO : end -->
 
-                    <!-- PROJECT INFO : begin -->
-                    <dl class="project-info">
-                        <dt>Author</dt>
-                        <dd><a href="http://justinmezzell.com/">Justin Mezzell</a></dd>
-                        <dt>Source</dt>
-                        <dd><a href="https://dribbble.com/JustinMezzell">dribbble.com</a></dd>
-                    </dl>
-                    <!-- PROJECT INFO : end -->
+                        <!-- PROJECT DESCRIPTION : begin -->
+                        <div class="project-description various-content">
+                            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+                            <p>It is a paradisematic country, in which roasted parts of sentences fly into your mouth. Even the all-powerful Pointing has no control about the blind texts it is an almost.</p>
+                        </div>
+                        <!-- PROJECT DESCRIPTION : end -->
 
-                    <!-- PROJECT DESCRIPTION : begin -->
-                    <div class="project-description various-content">
-                        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
-                        <p>It is a paradisematic country, in which roasted parts of sentences fly into your mouth. Even the all-powerful Pointing has no control about the blind texts it is an almost.</p>
+                        <!-- PROJECT FOLDERS : begin -->
+                        <div class="project-folders">
+                            <ul class="project-categories">
+                                <li><a href="#">Art</a></li>
+                                <li><a href="#">Illustration</a></li>
+                                <li><a href="#">Print</a></li>
+                            </ul>
+                            <ul class="project-tags">
+                                <li><a href="#">dribbble</a></li>
+                                <li><a href="#">shot</a></li>
+                            </ul>
+                        </div>
+                        <!-- PROJECT FOLDERS : end -->
+
                     </div>
-                    <!-- PROJECT DESCRIPTION : end -->
-
-                    <!-- PROJECT FOLDERS : begin -->
-                    <div class="project-folders">
-                        <ul class="project-categories">
-                            <li><a href="#">Art</a></li>
-                            <li><a href="#">Illustration</a></li>
-                            <li><a href="#">Print</a></li>
-                        </ul>
-                        <ul class="project-tags">
-                            <li><a href="#">dribbble</a></li>
-                            <li><a href="#">shot</a></li>
-                        </ul>
-                    </div>
-                    <!-- PROJECT FOLDERS : end -->
-
                 </div>
             </div>
+
         </div>
+        <!-- PORTFOLIO DETAIL : end -->
 
     </div>
-    <!-- PORTFOLIO DETAIL : end -->
-
-</div>
-<!-- PAGE CONTENT : end -->
+    <!-- PAGE CONTENT : end -->
+</script>
